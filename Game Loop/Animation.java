db4c9f56a0fe68a18801413b448e7f0dfb020959
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-02 21:34:48 -0400

    Moved JFrame setup to AnimationEx.java

--- Game Loop/Animation.java
+++ Game Loop/Animation.java
@@ -6,7 +6,7 @@ public class Animation extends JPanel {
     int x = 0; // starting position (top left corner)
     int y = 0;
 
-    private void move() {
+    public void cycle() {
         x += 1; // move 1 pixel each loop (every 10 ms)
         y += 1;
     }
@@ -20,18 +20,11 @@ public class Animation extends JPanel {
         g2d.fillOval(x, y, 30, 30); // draw filled oval at top left corner with diameter 30
     }
 
-    public static void main(String[] args) throws InterruptedException { // allows threads to interrupt tasks
-        JFrame frame = new JFrame("Ball animation"); // initalize view window with title
-        Animation animation = new Animation(); // initalize game loop
-        frame.add(animation);
-        frame.setSize(400,400); // window size 400 x 400
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+    public void run() throws InterruptedException {
         for (;;) {
-            animation.move(); // update x y pos of ball
-            animation.repaint(); // paint new pos and delete paint of old pos
-            Thread.sleep(10); // loop every 10ms
+            cycle();
+            repaint();
+            Thread.sleep(10);
         }
     }
 }
