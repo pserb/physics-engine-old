1150018c8d5a4c0b6e265e052a7bbf6d3aa1ece7
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-12 00:28:45 -0400

    Used OOP to create a Ball object and draw it to JPanel

--- OOP Tests/Animation.java
+++ OOP Tests/Animation.java
@@ -6,7 +6,10 @@ public class Animation extends JPanel {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+        
+        // x, y, width, height, color
         Ball ball1 = new Ball((Graphics2D) g, 1, 1, 50, 50);
+        Ball ball2 = new Ball((Graphics2D) g, 51, 1, 50, 50);
     }
 
     public void run() throws InterruptedException {
--- OOP Tests/Ball.java
+++ OOP Tests/Ball.java
@@ -8,6 +8,7 @@ public class Ball extends JPanel {
     private int y;
     private int width;
     private int height;
+    // private String color;
     
     public Ball(Graphics2D g2d, int x, int y, int width, int height) {
         this.g2d = g2d;
@@ -15,8 +16,10 @@ public class Ball extends JPanel {
         this.y = y;
         this.width = width;
         this.width = height;
+        // this.color = color;
 
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // turn on antialiasing
+        g2d.setColor(new Color(100, 100, 200));;
         g2d.fillOval(x, y, width, height);
     }
 }
