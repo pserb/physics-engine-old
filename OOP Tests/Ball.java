7d3bd1bea1d3c37a74915ab7e10055d536af2e20
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-13 01:13:03 -0400

    greatly simplified implementation

--- OOP Tests/Ball.java
+++ OOP Tests/Ball.java
@@ -2,24 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Ball extends JPanel {
-
-    private Graphics2D g2d;
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-    // private String color;
     
-    public Ball(Graphics2D g2d, int x, int y, int width, int height) {
-        this.g2d = g2d;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.width = height;
-        // this.color = color;
-
+    public Ball(Graphics2D g2d, int x, int y, int width, int height, int r, int g, int b) {
+        
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // turn on antialiasing
-        g2d.setColor(new Color(100, 100, 200));;
+        g2d.setColor(new Color(r, g, b));
         g2d.fillOval(x, y, width, height);
+        
     }
 }
