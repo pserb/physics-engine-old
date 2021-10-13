b4b391b081fb5eacdc7f578e3af1cdd9b2d67b6d
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-13 01:12:38 -0400

    create method construct() that creates array of Balls

--- OOP Tests/Animation.java
+++ OOP Tests/Animation.java
@@ -3,13 +3,22 @@ import java.awt.*;
 
 public class Animation extends JPanel {
 
+    public Ball[] construct(Graphics g) {
+        Ball[] ballList = new Ball[10];
+
+        for (int i = 0; i <= 9; i++) {
+            // x, y, width, height, r, g, b
+            ballList[i] = new Ball((Graphics2D) g, 1 + (50 * i), 1, 50, 50, 50 + (i * 10), 100, 100);
+        }
+
+        return ballList;
+    }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        
-        // x, y, width, height, color
-        Ball ball1 = new Ball((Graphics2D) g, 1, 1, 50, 50);
-        Ball ball2 = new Ball((Graphics2D) g, 51, 1, 50, 50);
+
+        construct(g);
     }
 
     public void run() throws InterruptedException {
