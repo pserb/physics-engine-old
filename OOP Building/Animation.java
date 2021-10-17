294d4d05a82c4f23d6ce945b463086679c3b95b8
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-16 22:31:50 -0400

    Update Animation.java

--- OOP Building/Animation.java
+++ OOP Building/Animation.java
@@ -6,6 +6,8 @@ public class Animation extends JPanel {
 
     int numBalls = 10;
 
+    int time = 0;
+
     public ArrayList<Ball> construct() {
 
         ArrayList<Ball> balls = new ArrayList<Ball>();
@@ -13,11 +15,18 @@ public class Animation extends JPanel {
         for (int i = 0; i < numBalls; i++) {
 
             int dx = new Random().nextInt(900);
-            int dy = new Random().nextInt(900);
-            int dwidth = 30 + new Random().nextInt(50);
-            int dheight = 30 + new Random().nextInt(50);
+            int dy = new Random().nextInt(400);
+            int dwidth = 50;
+            int dheight = 50;
 
-            balls.add(new Ball(dx, dy, dwidth, dheight));
+            int dr = new Random().nextInt(255);
+            int dg = new Random().nextInt(255);
+            int db = new Random().nextInt(255);
+
+            int dtranslatex = 0;
+            int dtranslatey = 1;
+
+            balls.add(new Ball(dx, dy, dwidth, dheight, dr, dg, db, dtranslatex, dtranslatey));
 
         }
         return balls;
@@ -32,8 +41,11 @@ public class Animation extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // turn on antialiasing
 
+        g2d.fillRect(0, 920, 1000, 50);
+
         for (Ball ball : ballList) {
             ball.draw(g2d);
+            ball.move();
         }
 
     }
@@ -42,6 +54,10 @@ public class Animation extends JPanel {
         for (;;) {
             repaint();
             Thread.sleep(10);
+            time += 10;
+            if (time % 1000 == 0) {
+                System.out.println(time/1000);
+            }
         }
     }
 }
