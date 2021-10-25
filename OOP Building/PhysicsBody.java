a24ca7f26e7f195788c37f1233340a460f9d620d
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-25 03:36:48 -0400

    Create Interface

---
+++ OOP Building/PhysicsBody.java
@@ -0,0 +1,10 @@
+import java.awt.Graphics2D;
+import java.util.*;
+import java.awt.*;
+
+public interface PhysicsBody {
+    int getY();
+    void bounce();
+    void move();
+    void draw(Graphics2D g2d);
+}
