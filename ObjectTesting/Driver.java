570ff7ce99cc015fb968b114be4974194b31c937
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-26 23:09:21 -0400

    Update Driver.java

--- ObjectTesting/Driver.java
+++ ObjectTesting/Driver.java
@@ -1,12 +1,27 @@
 import java.util.*;
 
 public class Driver {
+
 	public static void main(String[] args) {
-	ArrayList<Object> list = new ArrayList<>();
+		
+		//==== ArrayList ===//
+		ArrayList<Object> list = new ArrayList<>();
 		for (int i = 0; i < 10; i++) {
 			list.add(new Object(new Random().nextInt(10), new Random().nextInt(10)));
 		}
 		
-		list.forEach(guy -> System.out.println(guy));
+		//==== Check Matches ====//
+		int matches = 0;
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+
+			for (int j = i+1; j < list.size(); j++) {
+				if ((list.get(i).equalsX(list.get(j))) && (list.get(i).equalsY(list.get(j)))) {
+					matches++;
+				}
+			}
+		}
+		System.out.println("\nThere Are: " + matches + " matches.");
 	}
 }
