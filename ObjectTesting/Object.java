7d5c314817837b1c7df61f72fbc33debf181fb78
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-10-26 23:09:23 -0400

    Update Object.java

--- ObjectTesting/Object.java
+++ ObjectTesting/Object.java
@@ -7,6 +7,14 @@ public class Object {
 		this.x = x;
 		this.y = y;
 	}
+
+	public boolean equalsX(Object other) {
+		return (x == other.x);
+	}
+
+	public boolean equalsY(Object other) {
+		return (y == other.y);
+	}
 	
 	public String toString() {
 		return "(" + x + ", " + y + ")";
